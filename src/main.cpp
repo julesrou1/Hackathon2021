e18d7fcdbbb2650d56f9eba6433ecce5f0fee476
julesrou1 <julesrou123@gmail.com>
2021-01-30 13:56:21 +0100

    small serial fix

--- src/main.cpp
+++ src/main.cpp
@@ -174,18 +174,18 @@ void loop() {
       return;
     }
     //Show UID on serial monitor
-    Serial.print("UID tag :");
+    // Serial.print("UID tag :");
     String content = "";
     //byte letter;
     for (byte i = 0; i < mfrc522.uid.size; i++)
     {
-      Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
-      Serial.print(mfrc522.uid.uidByte[i], HEX);
+      // Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+      // Serial.print(mfrc522.uid.uidByte[i], HEX);
       content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
       content.concat(String(mfrc522.uid.uidByte[i], HEX));
     }
-    Serial.println();
-    Serial.print("Message : ");
+    // Serial.println();
+    // Serial.print("Message : ");
     content.toUpperCase();
     if (content.substring(1) == AuthorsizedCardUID) // on peut changer la cle d'acces ici
     {
